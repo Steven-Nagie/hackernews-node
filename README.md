a0ecc369c8ed85c7bf9774048084a4e09cb06084
steven <sanagie@yahoo.com>
2019-01-06 13:37:37 -0700

    Resolver functions for feed and post connected to prisma. Find link by id broken

---
+++ README.md
@@ -0,0 +1,1 @@
+As of January 6, got to this point in tutorial: https://www.howtographql.com/graphql-js/6-authentication/
--- src/index.js
+++ src/index.js
@@ -1,70 +1,46 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client');
 
-
-// 1
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
-
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      const id = args.id;
-      let link;
-      links.forEach(innerLink => {
-        if (innerLink.id === id) {
-          link = innerLink;
-        }
-      });
-      return link;
+    feed: (root, args, context, info) => context.prisma.links(),
+    link: (root, args, context) => {
+      return context.prisma.link(args.id)
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     },
-    updateLink: (parent, args) => {
-      for (let i = 0; i < links.length; i++) {
-        if (links[i].id === args.id) {
-          links[i].url = args.url ? args.url : links[i].url;
-          links[i].description = args.description ? args.description : links[i].description;
-          return links[i];
-        }
-      }
-      return null;
-    },
-    deleteLink: (parent, args) => {
-      for (let i = 0; i < links.length; i++) {
-        if (links[i].id === args.id) {
-          links.splice(i, 1);
-        }
-        return links;
-      }
-    }
+    // updateLink: (parent, args) => {
+    //   for (let i = 0; i < links.length; i++) {
+    //     if (links[i].id === args.id) {
+    //       links[i].url = args.url ? args.url : links[i].url;
+    //       links[i].description = args.description ? args.description : links[i].description;
+    //       return links[i];
+    //     }
+    //   }
+    //   return null;
+    // },
+    // deleteLink: (parent, args) => {
+    //   for (let i = 0; i < links.length; i++) {
+    //     if (links[i].id === args.id) {
+    //       links.splice(i, 1);
+    //     }
+    //     return links;
+    //   }
+    // }
   }
-  // Link: {
-  //   id: (parent) => parent.id,
-  //   description: (parent) => parent.description,
-  //   url: (parent) => parent.url
-  // }
 }
 
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-})
+  context: { prisma }
+});
 server.start(() => console.log(`Server is running on http://localhost:4000`))
